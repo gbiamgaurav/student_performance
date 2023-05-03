7b22f2056ef8c281c19d4b18b3afaa8661eb46fc
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-05-03 21:06:59 +0530

    Stage02 data_transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -36,7 +38,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -51,5 +53,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     
-
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    
 
