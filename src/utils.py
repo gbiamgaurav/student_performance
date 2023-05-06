515fbd3ed571456767637f4ce74e340b9f22d8d7
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-05-06 19:40:59 +0530

    app.py added

--- src/utils.py
+++ src/utils.py
@@ -98,7 +98,5 @@ def load_object(file_path):
     except Exception as e:
         raise CustomException(e, sys)
     
-    except Exception as e:
-        raise CustomException(e, sys)
             
             
